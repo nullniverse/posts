94e7979e6c12deec0fbabb9ddd9afb44d013f01f
Doug <nullniverse@pm.me>
2026-02-01 19:10:06 -0300

    fix: adjusting blog theming

--- src/consts.ts
+++ src/consts.ts
@@ -2,12 +2,11 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_URL = "https://blog.16090000.xyz";
-export const SITE_TITLE = "Nullniverse's blog";
-export const SITE_DESCRIPTION =
-  "Personal blog in the miniblog spirit!";
+export const SITE_TITLE = "Nullniverse's Blog";
+export const SITE_DESCRIPTION = "Posts and stuff";
 
-export const EMAIL = "boverflow@bk.ru";
-export const WIKI = "https://wiki.16090000.xyz";
+export const EMAIL = "nullniverse@temaocyber.cn";
+// export const WIKI = "https://wiki.16090000.xyz";
 export const GARDEN = "https://derelict.garden/enter.html";
 export const GARDEN_NEXT = "https://derelict.garden/next/blog.16090000.xyz";
 export const GARDEN_PREV = "https://derelict.garden/prev/blog.16090000.xyz";
